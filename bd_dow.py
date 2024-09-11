c35ef50937bfc193be0d839ac46aeb6a606029d8
Paddfoot <galimzyanov0501@gmail.com>
2024-09-11 23:23:20 +0400

    v

--- bd_dow.py
+++ bd_dow.py
@@ -4,7 +4,7 @@ import codecs
 import os
 
 def dow(d):
-    url = "https://enka.network/ui/UI_Gacha_AvatarImg_"+d+".webp"
+    url = "https://enka.network/ui/UI_AvatarIcon_"+d+".png"
     res = requests.get(url).content
     with open('data/characters/'+ d + '/' + url.split('/')[-1].split('?')[0], 'wb') as f:
         f.write(res)
@@ -17,9 +17,9 @@ jess_dict = json.loads(text)
 i = 0
 
 while i < 89:
-    print(jess_dict['data']['characters'][i]['name'])
+    print(jess_dict['data']['characters'][i]['nid'])
     os.makedirs('data/characters/' + jess_dict['data']['characters'][i]['name'], exist_ok=True)
-    dow(jess_dict['data']['characters'][i]['name'])
+    dow(jess_dict['data']['characters'][i]['nid'])
     i = i +1
 
 fileObj.close()
--- ff0.py
+++ ff0.py
@@ -1,10 +1,29 @@
 from PIL import Image
 import os
 
+#directory = "data/characters/"
+#files = os.listdir(directory)
+#for i in files:
+#    image_path = 'data/characters/' + i + '/UI_NameCardPic_' + i + '_P.jpg'
+#    # указываем фиксированный размер стороны
+#    fixed_width = 140
+#    img = Image.open(image_path)
+#    # получаем процентное соотношение
+#    # старой и новой ширины
+#    width_percent = (fixed_width / float(img.size[0]))
+#    # на основе предыдущего значения
+#    # вычисляем новую высоту
+#    height_size = int((float(img.size[0]) * float(width_percent)))
+#    # меняем размер на полученные значения
+#    new_image = img.resize((140, 67))
+#    #new_image.show()
+#    new_image.save('data/characters/' + i + '/UI_NameCardPic_' + i + '_P_min.jpeg', "JPEG", quality=100, optimize=True, progressive=True)
+#    print(i)
+
 directory = "data/characters/"
 files = os.listdir(directory)
 for i in files:
-    image_path = 'data/characters/' + i + '/UI_NameCardPic_' + i + '_P.jpg'
+    image_path = 'data/characters/' + i + '/UI_AvatarIcon_'+i+'.png'
     # указываем фиксированный размер стороны
     fixed_width = 140
     img = Image.open(image_path)
@@ -15,7 +34,7 @@ for i in files:
     # вычисляем новую высоту
     height_size = int((float(img.size[0]) * float(width_percent)))
     # меняем размер на полученные значения
-    new_image = img.resize((140, 67))
+    new_image = img.resize((66, 66))
     #new_image.show()
-    new_image.save('data/characters/' + i + '/UI_NameCardPic_' + i + '_P_min.jpeg', "JPEG", quality=100, optimize=True, progressive=True)
+    new_image.save('data/characters/' + i + '/UI_AvatarIcon_'+i+'_min.png')
     print(i)
